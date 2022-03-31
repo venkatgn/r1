82239558b1c675d42e545b6429e786e2e4698099
vgutta <vgutta@INDHYDELP143.planit.local>
2022-03-31 09:53:34 +0530

    base updated

--- pr1/src/main/java/base/Base.java
+++ pr1/src/main/java/base/Base.java
@@ -1,5 +1,12 @@
 package base;
 
-public class Base {
+import org.testng.annotations.Test;
 
+public class Base 
+{
+  @Test
+  public void setup()
+  {
+	  System.out.println("this is setup");
+  }
 }
